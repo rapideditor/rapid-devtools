c0750eac6b2a88a609c92fe933422183472a8bcb
Queen Belle V. Dela Cruz <qb.dcruz@gmail.com>
2024-06-19 11:38:34 -0700

    Initial refactoring for manifest v3

--- devtools.js
+++ devtools.js
@@ -3,4 +3,9 @@
 // chrome.extension.*
 
 // Create a tab in the devtools area
-chrome.devtools.panels.create("Rapid", "toast.png", "panel.html", function(panel) {});
+chrome.devtools.panels.create("Rapid",              //title
+                              "rapid_favicon.svg",  //iconPath
+                              "panel.html",         //pagePath
+                              function(panel) {});
+
+console.log(window.rapidContext)
