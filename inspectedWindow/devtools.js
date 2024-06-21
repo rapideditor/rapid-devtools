671e7c0916e84041017bd84b9d6a494ae2668d1b
Queen Belle V. Dela Cruz <qb.dcruz@gmail.com>
2024-06-21 12:03:14 -0700

    Use insepected window skeleton

---
+++ inspectedWindow/devtools.js
@@ -0,0 +1,32 @@
+
+const page_getProperties = function () {
+  let data = window.jQuery && $0 ? jQuery.data($0) : {};
+  // Make a shallow copy with a null prototype, so that sidebar does not
+  // expose prototype.
+  let props = Object.getOwnPropertyNames(data);
+  let copy = { __proto__: null };
+  for (let i = 0; i < props.length; ++i) copy[props[i]] = data[props[i]];
+  return copy;
+};
+
+//Create panel
+chrome.devtools.panels.create(
+  'Rapid',
+  'assets/rapid_favicon.png',
+  'panel.html',
+  () => {}
+);
+
+//Create sidebar
+chrome.devtools.panels.elements.createSidebarPane(
+  'Rapid Properties',
+  (sidebar) => {
+    function updateElementProperties() {
+      sidebar.setExpression('(' + page_getProperties.toString() + ')()');
+    }
+    updateElementProperties();
+    chrome.devtools.panels.elements.onSelectionChanged.addListener(
+      updateElementProperties
+    );
+  }
+);
