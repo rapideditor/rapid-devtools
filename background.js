f080eec9979a6c6c5a8cfe97466df1413914ef66
Queen Belle V. Dela Cruz <qb.dcruz@gmail.com>
2024-07-02 09:30:47 -0700

    Update devtools formatting

--- background.js
+++ background.js
@@ -1,6 +1,5 @@
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === "sendElementProperties") {
-      // Forward the message to panel.js
       chrome.runtime.sendMessage({ action: "receiveElementProperties", data: message.data });
   }
 });
